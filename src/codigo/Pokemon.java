eaaca973ca7ce7a4c2311f315fa28add409be36a
Alberto <Alberto@LAPTOP-RD4DMT3L>
2020-02-20 15:39:21 +0100

    Se visualizan los pokemon y hay HashMap

---
+++ src/codigo/Pokemon.java
@@ -0,0 +1,22 @@
+package codigo;
+
+/**
+ *
+ * @author Alberto Sanz
+ */
+//Se declaran todos los parametros que qudemos conocer a la hora de hacer las consultas
+public class Pokemon {
+
+    public int ID = 0; 
+    public String Nombre = "";
+    public String Especie = "";
+    public String Movimiento1 = "";
+    public int preEvolucion = 0;
+    public int posEvolucion = 0;
+    public String Tipo = "";
+    public double Altura = 0;
+    public int Numero = 0;
+    public double Peso = 0.0;
+    
+
+}
