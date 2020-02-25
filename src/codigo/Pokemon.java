e8617a0da56c635d554dd89b5a17538f1c7a31b5
Alberto <Alberto@LAPTOP-RD4DMT3L>
2020-02-25 20:07:30 +0100

    Nombre, descripcion, peso, altura e ID FIN

--- src/codigo/Pokemon.java
+++ src/codigo/Pokemon.java
@@ -4,20 +4,24 @@ package codigo;
  *
  * @author Alberto Sanz
  */
-//Se declaran todos los parametros que qudemos conocer a la hora de hacer las consultas
+//Se declaran todos los parametros que queremos conocer a la hora de hacer las consultas
 public class Pokemon {
 
-    public String id = ""; 
+    public String id = "";
     public String nombre = "";
-    public String descripcion ="";
+    public String altura = "";
+    public String peso = "";
     public String especie = "";
+    public String habitat = "";
+    public String tipo1 = "";
+    public String tipo2 = "";
+    public String habilidad = "";
     public String movimiento1 = "";
+    public String movimiento2 = "";
+    public String movimiento3 = "";
+    public String movimiento4 = "";
     public String preEvolucion = "";
     public String posEvolucion = "";
-    public String tipo = "";
-    public String altura = "";
-    public String numero = "";
-    public String peso = "";
-    
+    public String descripcion = "";
 
 }
